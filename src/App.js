96612bf5ac8471a391b1b309fe7a8fed76ebbe6b
cjtakhar <cjtakhar@gmail.com>
2023-05-09 22:06:35 -0700

    1.3.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/msg-board" element={<Dash />} />
+          <Route path="/msgboard" element={<Dash />} />
         </Routes>
       </BrowserRouter>
     </div>
