e4b10fd58a78590dd2d6c6d6f95ffbe71ebe4714
cjtakhar <cjtakhar@gmail.com>
2023-05-12 19:00:35 -0700

    update

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
             })
             const data = await response.json()
             if (data.success) {
-                window.location.href = '/welcome'
+                window.location.href = '/msg'
                 setErrorMessage('')
             } else {
                 setErrorMessage('Incorrect username or password')
